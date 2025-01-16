fa3e7e357ffddfb52c8752c692f18b3f00e5c224
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-16 08:07:48 +0000

    chore: fix code style

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,7 +21,7 @@ const showingNavigationDropdown = ref(false)
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationLogo class="block text-gray-800 h-9 w-auto fill-current " />
+                                    <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
 
