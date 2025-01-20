88afee61efae414f6e3de5d657f62dabceee8ec9
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-21 01:13:45 +0600

    Booking, Rental, Location, User and activity logs module added

---
+++ config/cities.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    [
+        'name' => 'Sylhet',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Dhaka',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Chittagong',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Khulna',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Rajshahi',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Barisal',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Comilla',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Narayanganj',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Mymensingh',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+    [
+        'name' => 'Rangpur',
+        'timezone' => 'Asia/Dhaka',
+        'country' => 'Bangladesh',
+        'country_code' => 'BD',
+    ],
+];
