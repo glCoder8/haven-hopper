a6e134c15d22733b2f0874a4e0e5b9545cdb7af7
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-26 16:30:24 +0600

    feature: date range picker added

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -1,20 +1,28 @@
 <script setup>
+import DateRangePicker from '@/Components/DateRangePicker.vue'
 import HomeNavigation from '@/Components/HomeNavigation.vue'
+import { useForm } from '@inertiajs/vue3'
 defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    rentals: {
-        type: Array,
-    },
+    canLogin: Boolean,
+    canRegister: Boolean,
+    rentals: Array,
 })
+
+const form = useForm({
+    checkInDate: '',
+    checkOutDate: '',
+})
+
+const submitForm = () => {
+    console.log('Form Data:', form)
+}
 </script>
 
 <template>
     <HomeNavigation :can-login :can-register />
+    {{ form }}
+    <div>
+        <DateRangePicker v-model:checkInDate="form.checkInDate" v-model:checkOutDate="form.checkOutDate" />
+    </div>
+    <button @click="submitForm">Submit</button>
 </template>
-
-<style scoped></style>
