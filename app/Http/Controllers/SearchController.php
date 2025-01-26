dc843386c1711b17c6c6706b4a7200c6126ea479
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-26 23:15:49 +0600

    working on search feature

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Inertia\Response;
 
 class SearchController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): Response
     {
         return inertia()->render('Search', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
-            'rentals' => []
+            'rentals' => [],
         ]);
     }
 }
