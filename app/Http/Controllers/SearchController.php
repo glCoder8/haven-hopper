df1906e7ef04e154d6588506319106dce0e394b2
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-27 23:32:35 +0600

    Refactored Search Controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CityResource;
 use App\Models\Rental;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Inertia\Response;
 
 class SearchController extends Controller
@@ -15,22 +14,22 @@ class SearchController extends Controller
      */
     public function __invoke(Request $request): Response
     {
-        $query = Rental::query()->with(['location', 'amenities']);
-
-        if ($request->query('total_guests')) {
-            $query = $query->where('total_guests', $request->query('total_guests'));
-        }
-
-        if ($request->query('city')) {
-            $query = $query->whereHas('location', function ($query) use ($request) {
-                return $query->where('city', $request->query('city'));
-            });
-        }
+        $rentals = Rental::query()
+            ->with(['location', 'amenities'])
+            ->when(
+                $request->query('total_guests'),
+                fn ($query, $guestsCount) => $query->where('total_guests', intval($guestsCount))
+            )
+            ->when(
+                $request->query('city'),
+                fn ($query, $cityName) => $query->where('city', $cityName)
+            )
+            ->latest()
+            ->paginate($request->per_page ?? 6)
+            ->withQueryString();
 
         return inertia()->render('Search', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'rentals' => $query->latest()->paginate(6),
+            'rentals' => $rentals,
             'cities' => CityResource::collection(config('cities')),
             'filters' => $request->query(),
         ]);
