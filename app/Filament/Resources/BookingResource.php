b73479c129bac95a85b07a1c0de4e38b5b196e0d
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-29 16:36:39 +0600

    bookings are visible from admin panel

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -2,59 +2,67 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\BookingPaymentStatus;
+use App\Enums\BookingStatus;
 use App\Filament\Resources\BookingResource\Pages;
 use App\Models\Booking;
-use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class BookingResource extends Resource
 {
     protected static ?string $model = Booking::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\DateTimePicker::make('check_in_date')
-                    ->required(),
-                Forms\Components\DateTimePicker::make('check_out_date')
-                    ->required(),
-                Forms\Components\TextInput::make('total_guests')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('price')
+                TextInput::make('total_guests')
                     ->required()
                     ->numeric()
+                    ->default(1),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->default(0)
                     ->prefix('$'),
-                Forms\Components\TextInput::make('total_price')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('discount')
+                TextInput::make('discount')
                     ->required()
                     ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('tax')
+                    ->default(0)
+                    ->prefix('$'),
+                TextInput::make('tax')
                     ->required()
                     ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('convenience_fee')
+                    ->default(0)
+                    ->prefix('$'),
+                TextInput::make('convenience_fee')
                     ->required()
                     ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('status')
+                    ->default(0)
+                    ->prefix('$'),
+                TextInput::make('total_price')
+                    ->required()
+                    ->numeric()
+                    ->default(0)
+                    ->prefix('$'),
+                Select::make('status')
+                    ->required()
+                    ->options(BookingStatus::class)
+                    ->label('Approval Status'),
+                TextInput::make('payment_status')
                     ->required(),
-                Forms\Components\TextInput::make('payment_status')
-                    ->required(),
-                Forms\Components\Select::make('user_id')
+                Select::make('user_id')
                     ->relationship('user', 'name')
-                    ->required(),
-                Forms\Components\Select::make('rental_id')
-                    ->relationship('rental', 'title')
                     ->required(),
             ]);
     }
@@ -63,51 +71,70 @@ class BookingResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('check_in_date')
-                    ->dateTime()
+                TextColumn::make('check_in_date')
+                    ->date()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('check_out_date')
-                    ->dateTime()
+                TextColumn::make('check_out_date')
+                    ->date()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('total_guests')
+                TextColumn::make('total_guests')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('price')
+                TextColumn::make('status')
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->color(fn (BookingStatus $state) => match ($state) {
+                        BookingStatus::APPROVED => 'success',
+                        BookingStatus::PENDING => 'pending',
+                        BookingStatus::REJECTED => 'danger',
+                        BookingStatus::CANCELLED => 'warning',
+                        BookingStatus::COMPLETED => 'info',
+                    }),
+                TextColumn::make('price')
                     ->money()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('total_price')
+                TextColumn::make('total_price')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('discount')
+                TextColumn::make('discount')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('tax')
+                TextColumn::make('tax')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('convenience_fee')
+                TextColumn::make('convenience_fee')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('payment_status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('user.name')
+                TextColumn::make('payment_status')
+                    ->sortable()
+                    ->badge()
+                    ->color(fn (BookingPaymentStatus $state) => match ($state) {
+                        BookingPaymentStatus::PAID => 'success',
+                        BookingPaymentStatus::PENDING => 'warning',
+                        BookingPaymentStatus::FAILED => 'danger',
+                    }),
+                TextColumn::make('user.name')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('rental.title')
+                TextColumn::make('rental.title')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
+                TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
+                TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                SelectFilter::make('payment_status')
+                    ->options(BookingPaymentStatus::class),
+                SelectFilter::make('status')
+                    ->options(BookingStatus::class)
+                    ->label('Approve Status'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -130,7 +157,6 @@ class BookingResource extends Resource
     {
         return [
             'index' => Pages\ListBookings::route('/'),
-            'create' => Pages\CreateBooking::route('/create'),
             'edit' => Pages\EditBooking::route('/{record}/edit'),
         ];
     }
