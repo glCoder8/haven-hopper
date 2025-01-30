a9dd0c7e41b4161789cc4568c6c4f08f41108378
glCoder8 <42347449+glCoder8@users.noreply.github.com>
2025-01-30 21:31:32 +0600

    feature: added total price for booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -12,6 +12,8 @@ use App\Models\Rental;
 use App\Models\User;
 use App\Services\PaymentService\PaymentFailedException;
 use App\Services\PaymentService\PaymentProcessor;
+
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -41,22 +43,25 @@ class BookingController extends Controller
 
     public function availabilityValidate(BookAvailabilityRequest $request, Rental $rental): RedirectResponse
     {
-        return redirect()->back()->with([
-            'message' => [
-                'body' => 'Booking Available',
-                'type' => 'success',
-            ],
-        ]);
+        return redirect()->back();
     }
 
     public function checkout(Request $request, Rental $rental): Response
     {
         $availablePaymentMethods = PaymentProcessor::availableProviders();
 
+        $requestCheckInDate= $request->query('checkInDate');
+        $requestCheckOutDate= $request->query('checkOutDate');
+
+        $totalStay =Carbon::createFromDate($requestCheckInDate)->diffInDays(Carbon::createFromDate($requestCheckOutDate));
+
+
         return inertia()->render('Checkout', [
             'rental' => new RentalResource($rental->load('location')),
-            'checkInDate' => $request->query('checkInDate'),
-            'checkOutDate' => $request->query('checkOutDate'),
+            'totalStay' => $totalStay,
+            'totalPrice' => ($totalStay * $rental->price),
+            'checkInDate' => $requestCheckInDate,
+            'checkOutDate' => $requestCheckOutDate,
             'availablePaymentMethods' => $availablePaymentMethods,
         ]);
     }
